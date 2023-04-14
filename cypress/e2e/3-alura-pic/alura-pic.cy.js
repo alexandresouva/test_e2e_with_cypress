a0b830f42b3e511fd6306996acad72c7a5fbeb59
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-04-14 12:22:52 -0300

    feat: add specific validations to fields

--- cypress/e2e/3-alura-pic/alura-pic.cy.js
+++ cypress/e2e/3-alura-pic/alura-pic.cy.js
@@ -14,4 +14,39 @@ describe('Login and user register at Alura Pic ', () => {
     cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
     cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
   });
+
+  it('Check if it is a valid e-mail', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="email"]').type('alexandre');
+    cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+  });
+
+  it('Check if full name length is less than 2', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="fullName"]').type('a').blur();
+    cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+  });
+
+  it('Check if user name length is less than 2', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="userName"]').type('a').blur();
+    cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+  });
+
+  it('Check if user name has uppercase', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="userName"]').type('Ale').blur();
+    cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
+  });
+
+  it('Checks if user name length is less than 8', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="password"]').type('alexe').blur();
+    cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+  });
 });
