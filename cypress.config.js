498649baf3cf6a8a1add09fb44f61b28d149c535
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-04-15 20:34:31 -0300

    build: add mochawesome and dashboard dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: '31is9o',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/report/mochawesome-report',
+    overwrite: true,
+    html: true,
+    json: false,
+    timestamp: 'mmddyyyy_HHMMss',
+  },
+  browser: 'chrome',
 });
